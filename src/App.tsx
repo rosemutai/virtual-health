859503edb101770167ee2a6cc7c0751d132d9dbe
rosemutai <chepngetichrose2030@gmail.com>
2023-02-18 22:27:53 +0300

    Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full h-screen bg-white">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
     </div>
   );
