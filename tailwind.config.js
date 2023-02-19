275cc820d0856f38a319097cd160be2c3e23ad00
rosemutai <chepngetichrose2030@gmail.com>
2023-02-19 13:57:52 +0300

    Add custom css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     extend: {
       colors: {
         'dark-blue': '#233348',
-        'light-blue': '#1F1534'
+        'light-blue': '#1F1534',
+        'light-grey': '#7D7987',
+        'blue': '#458FF6'
       }
     },
     fontFamily: {
