09fbfb92f46ac0ef39e200b86073643ce3050baf
rosemutai <chepngetichrose2030@gmail.com>
2023-02-19 13:57:00 +0300

    Add Services and Header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import TopHeader from './components/TopHeader';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 function App() {
   return (
-    <div className="App w-full h-screen bg-white md:pl-48 pt-8">
-      <header className="App-header">
-        <Navbar />
-      </header>
+    <div className="App w-full h-screen bg-white md:px-48 pt-8">
+      <Navbar />
+      <TopHeader />
+      <Services />
+      
     </div>
   );
 }
