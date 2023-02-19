f09bd2b7bcddb8254648aa29dfd5ed243cd252b3
rosemutai <chepngetichrose2030@gmail.com>
2023-02-19 14:05:22 +0300

    Add Services section

---
+++ src/components/Services.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='our-services flex flex-col'>
+      <div className='services-top'>
+        <h2 className='services-title text-black font-mulish font-bold text-3xl my-4'>Our Services</h2>
+        <hr className='border-b-black border w-14 text-center mx-auto mb-2'/>
+        <p className='text-light-grey font-mulish font-lg font-light text-center mix-blend-normal'>
+          We provide to you the best choiches for you. Adjust it to your 
+          health needs and make sure your undergo treatment with 
+          our highly qualified doctors you can consult with us which type
+          of service is suitable for your health
+        </p>
+      </div>
+      <section className="cards">
+        
+      </section>
+      
+    </div>
+  )
+}
+
+export default Services
