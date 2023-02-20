eb2dbdcfe2279b6d9bf5d57065b5012d815c9c97
rosemutai <chepngetichrose2030@gmail.com>
2023-02-20 19:00:38 +0300

    Add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
         'light-blue': '#1F1534',
         'light-grey': '#7D7987',
         'blue': '#458FF6'
+      },
+      backgroundImage: {
+        'hero-pattern': "url('../public/images/bgelement.svg')",
+      },
+
+      boxShadow: {
+        '3xl': '10, 40, 50, rgba(229, 233, 246, 0.4)'
       }
     },
     fontFamily: {
