6c98d7bd7a3d347d2f9dbee60399c63cda981ac2
rosemutai <chepngetichrose2030@gmail.com>
2023-02-21 18:27:55 +0300

    Modify spacing

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -3,7 +3,7 @@ import headerImage from '../images/headerillustration.svg'
 
 const TopHeader = () => {
   return (
-    <section className="about font-mulish w-full md:flex md:justify-between md:pt-8 bg-red-600">
+    <section className="about font-mulish w-full md:flex md:justify-between md:pt-8">
         <div className="description-text md:w-1/2 md:flex md:flex-col md:mt-6">
           <h1 className='font-bold text-3xl text-black mix-blend-normal md:w-5/6'>Virtual healthcare for you</h1>
           <p className='w-11/12 font-light text-sm text-light-grey md:py-6'>Trafalgar provides progressive, and affordable 
