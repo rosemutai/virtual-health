996be45eddaa993ae27d1e5f6ff0c0b48cc14d3a
rosemutai <chepngetichrose2030@gmail.com>
2023-02-24 20:09:24 +0300

    Fix footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,18 @@ import circle from '../images/ellipse7.svg'
 
 const Footer = () => {
   return (
-    <footer className='bg-footer-linearbackground w-full flex justify-between px-8'>
-      <div className="column-one about-company">
+    <footer className='bg-footer-linearbackground w-full flex justify-between p-14 relative text-white font-mulish'>
+      <div className="column-one about-company flex flex-col justify-center">
         <div className="logo flex">
-          <img src={circle} alt="circle"  className='absolute bottom-0 left-3'/>
-          <a href="/" className='absolute bottom-2 left-7 z-30 text-white font-mulish'>T</a>
-          <a href="/" className='text-dark-blue pt-2 md:pl-16'>Trafalgar</a>
+          <img src={circle} alt="circle" className='absolute bottom-32 left-20'/>
+          <a href="/" className='absolute bottom-8-5 left-24 z-30 text-white font-mulish'>T</a>
+          <a href="/" className='font-bold pb-4 md:pl-20'>Trafalgar</a>
         </div>
         <p className='text-white font-mulish font-light text-sm'></p>
         <p className='copyright text-sm font-mulish font-light'>©Trafalgar PTY LTD 2020. All rights reserved</p>
       </div>
       <div className="column-two flex flex-col">
-        <h5>Company</h5>
+        <h5 className='font-bold text-sm text-white py-4'>Company</h5>
         <ul className="nav-links">
           <li><a href="/">About</a></li>
           <li><a href="/testimonials">Testimonials</a></li>
@@ -24,7 +24,7 @@ const Footer = () => {
       </div>
 
       <div className="column-three flex flex-col">
-        <h5>Region</h5>
+        <h5 className='font-bold text-sm text-white'>Region</h5>
         <ul className="nav-links">
           <li><a href="/">Indonesia</a></li>
           <li><a href="/testimonials">Singapore</a></li>
@@ -34,7 +34,7 @@ const Footer = () => {
       </div>
 
       <div className="column-three flex flex-col">
-        <h5>Help</h5>
+        <h5 className='font-bold text-sm text-white'>Help</h5>
         <ul className="nav-links">
           <li><a href="/">Help center</a></li>
           <li><a href="/testimonials">Contact support</a></li>
